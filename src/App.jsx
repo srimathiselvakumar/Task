00cb38e063d64d8c4cc4a6ada3069911826e682a
srimathiselvakumar <153313108+srimathiselvakumar@users.noreply.github.com>
2025-01-21 15:43:23 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,44 @@
-
-import './App.css'
-
-import Header from './Header'
-
-
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
-  
+  const [tasks, setTasks] = useState([]);
+  const [taskInput, setTaskInput] = useState("");
+
+  const addTask = () => {
+    if (taskInput.trim()) {
+      setTasks([...tasks, taskInput]);
+      setTaskInput("");
+    }
+  };
+
+  const deleteTask = (index) => {
+    const newTasks = tasks.filter((_, i) => i !== index);
+    setTasks(newTasks);
+  };
 
   return (
-    <>
-     <Header/>
-      
-    </>
-  )
+    <div className="container">
+      <h1>Shopping To-Do List</h1>
+      <input
+        type="text"
+        value={taskInput}
+        onChange={(e) => setTaskInput(e.target.value)}
+        placeholder="Add a work..."
+      />
+      <button onClick={addTask}>Add Task</button>
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            {task}
+            <button className="deleteBtn" onClick={() => deleteTask(index)}>
+              X
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
